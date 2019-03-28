01882dffcdff41b50cdb985704bdf447ef38c915
Noah Gleason <noah.gleason1@gmail.com>
2019-03-27 23:07:56 -0400

    Add in angle and linear feedforward

--- src/main/java/org/usfirst/frc/team449/robot/generalInterfaces/doubleUnaryOperator/feedForwardComponent/FeedForwardArmComponent.java
+++ src/main/java/org/usfirst/frc/team449/robot/generalInterfaces/doubleUnaryOperator/feedForwardComponent/FeedForwardArmComponent.java
@@ -11,6 +11,8 @@ public class FeedForwardArmComponent extends FeedForwardKaKvComponent {
 
     private final double angleOffset;
 
+    private final double cosTerm;
+
     /**
      * Field to avoid garbage collection
      */
@@ -24,8 +26,9 @@ public class FeedForwardArmComponent extends FeedForwardKaKvComponent {
      *                            Defaults to kVFwd.
      * @param kAFwd               The voltage required to accelerate the arm up at one rotation per second^2 with the arm horizontal. Defaults to zero, meaning we don't use acceleration feed-forward.
      * @param kARev               The voltage required to accelerate the arm down at one foot per second^2 with the arm horizontal. Defaults to kAFwd.
-     * @param interceptVoltageFwd The voltage required to overcome static friction in the forwards direction with the arm horizontal. Vintercept in the drive characterization paper. Defaults to 0.
-     * @param interceptVoltageRev The voltage required to overcome static friction in the reverse direction with the arm horizontal. Vintercept in the drive characterization paper. Defaults to interceptVoltageFwd.
+     * @param interceptVoltageFwd The voltage required to overcome static friction in the forwards direction with the arm vertical. Vintercept in the drive characterization paper. Defaults to 0.
+     * @param interceptVoltageRev The voltage required to overcome static friction in the reverse direction with the arm vertical. Vintercept in the drive characterization paper. Defaults to interceptVoltageFwd.
+     * @param cosTerm The extra voltage required to move the arm when it's horizontal vs vertical, in volts.
      * @param startingAngle The starting angle of the arm, in degrees. The angles are defined such that zero degrees is horizontal. Defaults to zero.
      */
     @JsonCreator
@@ -35,8 +38,10 @@ public class FeedForwardArmComponent extends FeedForwardKaKvComponent {
                                    @Nullable Double kARev,
                                    double interceptVoltageFwd,
                                    @Nullable Double interceptVoltageRev,
+                                   double cosTerm,
                                    double startingAngle) {
         super(kVFwd, kVRev, kAFwd, kARev, interceptVoltageFwd, interceptVoltageRev);
+        this.cosTerm = cosTerm;
         this.angleOffset = startingAngle/360.;
     }
 
@@ -44,16 +49,16 @@ public class FeedForwardArmComponent extends FeedForwardKaKvComponent {
     public double calcMPVoltage(double positionSetpoint, double velSetpoint, double accelSetpoint) {
         angleFactor = Math.cos((talon.getPositionFeet() + angleOffset)*2*Math.PI);
         if (velSetpoint > 0 || (velSetpoint == 0 && accelSetpoint > 0)) {
-            return velSetpoint * kVFwd + angleFactor * (accelSetpoint * kAFwd + interceptVoltageFwd);
+            return velSetpoint * kVFwd + angleFactor * accelSetpoint * kAFwd + interceptVoltageFwd + angleFactor * cosTerm;
         } else if (velSetpoint < 0 || (velSetpoint == 0 && accelSetpoint < 0)) {
-            return velSetpoint * kVRev + angleFactor * (accelSetpoint * kARev - interceptVoltageRev);
+            return velSetpoint * kVRev + angleFactor * accelSetpoint * kARev - interceptVoltageRev + angleFactor * cosTerm;
         } else {
-            return 0;
+            return angleFactor * cosTerm;
         }
     }
 
     @Override
     public double applyAsDouble(double operand) {
-        return super.applyAsDouble(operand) * Math.cos((talon.getPositionFeet() + angleOffset)*2*Math.PI);
+        return super.applyAsDouble(operand) + cosTerm * Math.cos((talon.getPositionFeet() + angleOffset)*2*Math.PI);
     }
 }
