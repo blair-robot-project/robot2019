822db601ca4cfea7f832072d5ef2e1d7a1a6af72
Nate Kelkay <natikelkay@gmail.com>
2019-03-29 21:39:42 -0400

    Fix java errors

--- src/main/java/org/usfirst/frc/team449/robot/commands/multiSubsystem/LowerIntakeRaise.java
+++ src/main/java/org/usfirst/frc/team449/robot/commands/multiSubsystem/LowerIntakeRaise.java
@@ -55,12 +55,12 @@ public class LowerIntakeRaise<T extends Subsystem & SubsystemIntake & SubsystemS
                             @Nullable SubsystemIntake.IntakeMode carriageHoldMode,
                             @Nullable Boolean raiseIntake) {
         addSequential(new SetSolenoid(actuatedIntake, intakeSolenoidPos));
-        addSequential(new GoToPosition<>(elevator, elevatorIntakePos));
-        addSequential(new SetIntakeMode(actuatedIntake, actuatedIntakeMode));
-        addSequential(new SetIntakeMode(carriage, carriageIntakeMode));
+        addSequential(new GoToPosition<>(elevator, elevatorIntakePos, 0 , null));
+        addSequential(new SetIntakeMode<>(actuatedIntake, actuatedIntakeMode));
+        addSequential(new SetIntakeMode<>(carriage, carriageIntakeMode));
         addSequential(new IntakeUntilConditonMet<>(carriage, carriageIntakeMode, carriageHoldMode));
-        addSequential(new SetIntakeMode(actuatedIntake, SubsystemIntake.IntakeMode.OFF));
-        addSequential(new GoToPosition<>(elevator, elevatorUpPos));
+        addSequential(new SetIntakeMode<>(actuatedIntake, SubsystemIntake.IntakeMode.OFF));
+        addSequential(new GoToPosition<>(elevator, elevatorUpPos, 0, null));
         //Retract the intake by setting the piston to the opposite
         if (raiseIntake != null && raiseIntake) {
             addSequential(new SetSolenoid(actuatedIntake,
--- src/main/java/org/usfirst/frc/team449/robot/subsystem/complex/intake/IntakeCurrentLimited.java
+++ src/main/java/org/usfirst/frc/team449/robot/subsystem/complex/intake/IntakeCurrentLimited.java
@@ -1,14 +1,17 @@
 package org.usfirst.frc.team449.robot.subsystem.complex.intake;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.usfirst.frc.team449.robot.jacksonWrappers.FPSTalon;
 import org.usfirst.frc.team449.robot.subsystem.interfaces.conditional.SubsystemConditional;
-import org.usfirst.frc.team449.robot.subsystem.interfaces.intake.SubsystemIntake;JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class)
+import org.usfirst.frc.team449.robot.subsystem.interfaces.intake.SubsystemIntake;
 
+@JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class)
 public class IntakeCurrentLimited extends Subsystem implements SubsystemIntake, SubsystemConditional {
 
     /**
@@ -151,6 +154,6 @@ public class IntakeCurrentLimited extends Subsystem implements SubsystemIntake, 
      */
     @Override
     public void update() {
-        this.condition = isConditionTrue()
+        this.condition = isConditionTrue();
     }
 }
