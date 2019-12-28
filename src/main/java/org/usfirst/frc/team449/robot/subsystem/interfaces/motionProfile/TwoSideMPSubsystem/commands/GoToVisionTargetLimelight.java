78183f3e3e10516df767cc7c73901f0a8b5a1acb
Justin Hudis <justin.hudis@gmail.com>
2019-12-28 11:08:26 -0500

    Cleanup, add timeout, add recursion end condition

--- src/main/java/org/usfirst/frc/team449/robot/subsystem/interfaces/motionProfile/TwoSideMPSubsystem/commands/GoToVisionTargetLimelight.java
+++ src/main/java/org/usfirst/frc/team449/robot/subsystem/interfaces/motionProfile/TwoSideMPSubsystem/commands/GoToVisionTargetLimelight.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.usfirst.frc.team449.robot.components.PathRequester;
 import org.usfirst.frc.team449.robot.other.Waypoint;
 import org.usfirst.frc.team449.robot.subsystem.interfaces.AHRS.SubsystemAHRS;
@@ -17,7 +18,7 @@ import org.usfirst.frc.team449.robot.subsystem.interfaces.motionProfile.TwoSideM
 import org.usfirst.frc.team449.robot.subsystem.interfaces.motionProfile.commands.GetPathFromJetson;
 
 /**
- * A command that drives the given subsystem to x=0,z=0,yaw=0.
+ * A command that drives the given subsystem to the vision target detected by the Limelight.
  */
 @JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class)
 public class GoToVisionTargetLimelight<T extends Subsystem & SubsystemMPTwoSides & SubsystemAHRS> extends CommandGroup {
@@ -27,12 +28,26 @@ public class GoToVisionTargetLimelight<T extends Subsystem & SubsystemMPTwoSides
      */
     private final NetworkTable table;
 
+    /**
+     * Offsets added to x and y, respectively, after receiving the raw values from the Limelight.
+     */
     private final double xOffset, yOffset;
 
     /**
      * Whether to run this command repeatedly.
      */
     private final boolean adaptive;
+
+    /**
+     * Whether a vision target was detected by the Limelight. Null until the pose is pulled.
+     */
+    @Nullable
+    private Boolean targetDetected;
+
+    /**
+     * The pose array given by the Limelight of the form {x, y, z, pitch, yaw, roll}.
+     */
+    private double[] camtran;
 
     /**
      * Default constructor
@@ -43,7 +58,10 @@ public class GoToVisionTargetLimelight<T extends Subsystem & SubsystemMPTwoSides
      * @param maxAccel      The maximum acceleration, in feet/(second^2)
      * @param maxJerk       The maximum jerk, in feet/(second^3)
      * @param deltaTime     The time between setpoints in the profile, in seconds.
+     * @param xOffset       Offset added to x after receiving the raw value from the Limelight.
+     * @param yOffset       Offset added to y after receiving the raw value from the Limelight.
      * @param adaptive      Whether to run this command repeatedly.
+     * @param timeout       How long this command is allowed to run. Should be set if using adaptive mode.
      */
     @JsonCreator
     public GoToVisionTargetLimelight(@NotNull @JsonProperty(required = true) T subsystem,
@@ -54,11 +72,15 @@ public class GoToVisionTargetLimelight<T extends Subsystem & SubsystemMPTwoSides
                                      @JsonProperty(required = true) double deltaTime,
                                      double xOffset,
                                      double yOffset,
-                                     boolean adaptive) {
+                                     boolean adaptive,
+                                     double timeout) {
         requires(subsystem);
+        if (timeout > 0) {
+            setTimeout(timeout);
+        }
         this.table = NetworkTableInstance.getDefault().getTable("limelight");
-        GetPathFromJetson getPath = new GetPathFromJetson(pathRequester, null, deltaTime, maxVel, maxAccel, maxJerk,
-                false);
+        GetPathFromJetson getPath = new GetPathFromJetson(
+                pathRequester, null, deltaTime, maxVel, maxAccel, maxJerk, false);
         GoToPositionRelative goToPositionRelative = new GoToPositionRelative<>(getPath, subsystem);
         goToPositionRelative.setWaypoints(this::getWaypoints);
         addSequential(goToPositionRelative);
@@ -72,50 +94,69 @@ public class GoToVisionTargetLimelight<T extends Subsystem & SubsystemMPTwoSides
      */
     @NotNull
     private Waypoint[] getWaypoints() {
-        System.out.println("X: " + getX());
-        System.out.println("Y: " + getY());
-        System.out.println("THETA: " + getTheta());
+        pullCamtran();
+        double x = getX(), y = getY(), theta = getTheta();
+
+        targetDetected = (x != xOffset);
+
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+        System.out.println("THETA: " + theta);
+
         Waypoint[] toRet = new Waypoint[1];
-        toRet[0] = new Waypoint(getX(), getY(), getTheta());
+        toRet[0] = new Waypoint(x, y, theta);
         return toRet;
+    }
+
+    /**
+     * Populate the camtran array by pulling the values from NetworkTables.
+     */
+    private void pullCamtran() {
+        camtran = table.getEntry("camtran").getDoubleArray(new double[] {0, 0, 0, 0, 0, 0});
     }
 
     /**
      * @return The relative X distance to the setpoint, in feet.
      */
     private double getX() {
-        //Not a typo, this is how our coordinate transform works
-        return table.getEntry("camtran").getDoubleArray(new double[] {0, 0, 0, 0, 0, 0})[0] + xOffset;
+        return camtran[0] + xOffset;
     }
 
     /**
      * @return The relative Y distance to the setpoint, in feet.
      */
     private double getY() {
-        //Not a typo, this is how our coordinate transform works
-        return table.getEntry("camtran").getDoubleArray(new double[] {0, 0, 0, 0, 0, 0})[1] + yOffset;
-
+        return camtran[1] + yOffset;
     }
 
     /**
      * @return The relative angular distance to the setpoint, in degrees.
      */
     private double getTheta() {
-        return Math.toDegrees(table.getEntry("camtran").getDoubleArray(new double[] {0, 0, 0, 0, 0, 0})[4]);
+        return Math.toDegrees(camtran[4]);
     }
 
     /**
-     * @return Whether a pose was received from the Limelight
+     * Stop the command group either for the normal reason or if no target was detected by the Limelight.
+     * @return if the command group is finished or no target was detected.
      */
-    public boolean wasPoseReceived() {
-        return getX() != 0;
+    @Override
+    protected boolean isFinished() {
+        return super.isFinished() || (targetDetected != null && !targetDetected);
     }
 
+    /**
+     * Run this command again if in adaptive mode and a target was detected.
+     */
     @Override
     protected void end() {
-        if (adaptive) {
+        //Avoid null pointer
+        if (targetDetected == null) {
+            targetDetected = false;
+        }
+
+        if (adaptive && targetDetected) {
             Scheduler.getInstance().add(this);
         }
-        super.end();
     }
 }
